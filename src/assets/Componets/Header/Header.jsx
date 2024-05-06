0ff15f50cda9aeb60ff68ff216cf17a24d50ba88
Ameen <ameencrews@example.com>
2024-05-07 00:33:23 +0530

    l

--- src/assets/Componets/Header/Header.jsx
+++ src/assets/Componets/Header/Header.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './header.scss'
+import React from "react";
+import "./header.scss";
 
 export const Header = () => {
   return (
-    <div className='header-main'>
-        <div className='h-inner'>
-            <img src="userlogo.png" alt="userlogo" />
-           <img src="David Nowak.png" alt="" />
-            <img src="Vector.png" alt="" />
-        </div>
+    <div className="header-main">
+      <div className="h-inner">
+        <img src="userlogo.png" alt="userlogo" />
+        <img src="David Nowak.png" alt="" />
+        <img src="Vector.png" alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/assets/Pages/Templates/Templates.jsx
+++ src/assets/Pages/Templates/Templates.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import './templates.scss'
-import { Header } from '../../Componets/Header/Header'
-import { Aside } from '../../Componets/Aside/Aside'
-import { Templatelist } from '../../Componets/Templatelist/Templatelist'
+import React from "react";
+import "./templates.scss";
+import { Header } from "../../Componets/Header/Header";
+import { Aside } from "../../Componets/Aside/Aside";
+import { Templatelist } from "../../Componets/Templatelist/Templatelist";
 export const Templates = () => {
   return (
-    <div className='main-component-div'>
-      <Header/>
-       <Aside/>
-       <div className='letter-div'>
-         <p>Templates</p>
-         <div className='btn-1'>
+    <div className="main-component-div">
+      <Aside />
+      <Header />
+
+      <div className="letter-div">
+        <p>Templates</p>
+        <div className="btn-1">
           <button>+ Add new</button>
-         </div>
-       </div>
-       <Templatelist/>
+        </div>
+      </div>
+      <Templatelist />
     </div>
-  )
-}
+  );
+};
