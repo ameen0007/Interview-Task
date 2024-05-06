192bad13fdb5746f86d02673f7ef41196f98d2b2
Ameen <ameencrews@example.com>
2024-05-06 21:22:27 +0530

    k

---
+++ src/assets/Componets/Templatelist/Templatelist.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./templatelist.scss";
+import { FaEye } from "react-icons/fa";
+export const Templatelist = () => {
+  return (
+    <div className="temp-list-main">
+
+        <div className="search-inner">
+            <div className="search-box">
+            <input type="text" placeholder="Search...."/>
+            <div className="img-1">
+          <img src="searchlogo.png" alt="" />
+          </div>
+            </div>
+          
+        
+        </div>
+        
+   
+
+        <div class="table-main">
+    <div class="inner-table">
+        <div class="column">Template name</div>
+        <div class="column">Template type</div>
+        <div class="column">Created by</div>
+        <div class="column">Status</div>
+        <div class="column">Actions</div>
+    </div>
+
+    <div class="loop-row">
+        <div class="column">Installation template</div>
+        <div class="column">Installation</div>
+        <div class="column">
+            <div class="img-2">
+                <img src="userlogo.png" alt="" />
+                <p>David Nowak</p>
+            </div>
+        </div>
+        <div class="column"><span>Active</span></div>
+        <div class="column"><FaEye /></div>
+    </div>
+    <div class="loop-row">
+        <div class="column">Installation template</div>
+        <div class="column">Installation</div>
+        <div class="column">
+            <div class="img-2">
+                <img src="userlogo.png" alt="" />
+                <p>David Nowak</p>
+            </div>
+        </div>
+        <div class="column"><span>Active</span></div>
+        <div class="column"><FaEye /></div>
+    </div>
+    <div class="loop-row">
+        <div class="column">Installation template</div>
+        <div class="column">Installation</div>
+        <div class="column">
+            <div class="img-2">
+                <img src="userlogo.png" alt="" />
+                <p>David Nowak</p>
+            </div>
+        </div>
+        <div class="column"><span>Active</span></div>
+        <div class="column"><FaEye /></div>
+    </div>
+    <div class="loop-row">
+        <div class="column">Installation template</div>
+        <div class="column">Installation</div>
+        <div class="column">
+            <div class="img-2">
+                <img src="userlogo.png" alt="" />
+                <p>David Nowak</p>
+            </div>
+        </div>
+        <div class="column"><span>Active</span></div>
+        <div class="column"><FaEye /></div>
+    </div>
+</div>
+
+
+
+    </div>
+  );
+};
--- src/assets/Pages/Templates/Templates.jsx
+++ src/assets/Pages/Templates/Templates.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import './templates.scss'
 import { Header } from '../../Componets/Header/Header'
 import { Aside } from '../../Componets/Aside/Aside'
+import { Templatelist } from '../../Componets/Templatelist/Templatelist'
 export const Templates = () => {
   return (
     <div className='main-component-div'>
       <Header/>
        <Aside/>
        <div className='letter-div'>
-         <h6>Templates</h6>
+         <p>Templates</p>
          <div className='btn-1'>
           <button>+ Add new</button>
          </div>
        </div>
-       
+       <Templatelist/>
     </div>
   )
 }
